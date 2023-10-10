76efd432e1cae86c26306fe97b8c72e13878ea01
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-10 11:20:43 -0700

    Completed

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,23 @@
 class GuessingGame():
+    def __init__(self,num):
+        self.num = num
+        self.solve = 'false'
+    def guess(self, guess):
+        if(guess == self.num):
+            self.solve = 'true'
+        elif(guess > self.num):
+            print("lower")
+        else:
+            print("higher")
+    def solved(self):
+        if(self.solve == 'true'):
+            print('Correct')
+        else:
+            print("False")
+         
+
+game = GuessingGame(10)
+game.guess(6)
+game.guess(11)
+game.guess(55)
+game.solved()
